2fb49fa7c3b06b36b47696ebf4c0613443a7fee0
smartcorestudio <studio@smartcore.ru>
2017-12-07 11:52:06 +0300

    Create ru.js

---
+++ lang/ru.js
@@ -0,0 +1,12 @@
+'use strict';
+
+(function (CKEDITOR) {
+    CKEDITOR.plugins.setLang('media', 'ru', {
+        alt: 'Альтернативный текст',
+        caption: 'Подпись',
+        info: 'Инфо',
+        menu: 'Свойства медиа',
+        title: 'Медиа',
+        urlMissing: 'отсутствует URL'
+    });
+})(CKEDITOR);
