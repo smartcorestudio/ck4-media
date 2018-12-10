deaacde213c125d0f5d7356728aa8a7c72e1ef31
smartcorestudio <studio@smartcore.ru>
2018-12-10 10:12:57 +0300

    Create ru.js
    Russian language

---
+++ lang/ru.js
@@ -0,0 +1,17 @@
+'use strict';
+
+(function (CKEDITOR) {
+    CKEDITOR.plugins.setLang('media', 'ru', {
+        alt: 'Альтернативный текст',
+        audio: 'Аудио',
+        caption: 'Подпись',
+        iframe: 'Iframe',
+        image: 'Изображение',
+        info: 'Инфо',
+        link: 'Ссылка',
+        title: 'Медиа - Изображение, Аудио, Видео, Iframe',
+        type: 'Тип',
+        validateRequired: 'Это поле обязательно для заполнения,
+        video: 'Видео'
+    });
+})(CKEDITOR);
