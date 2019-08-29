6b8252f2eb9004861b8ff8261751b64dd26ede01
Mykola Pukhalskyi <notfound.la2.asterios.tm@gmail.com>
2019-08-29 15:59:58 +0300

    Create uk.js

---
+++ lang/uk.js
@@ -0,0 +1,12 @@
+'use strict';
+
+(function (CKEDITOR) {
+    CKEDITOR.plugins.setLang('media', 'uk', {
+        alt: 'Альтернативний текст',
+        caption: 'Підпис',
+        info: 'Інфо',
+        menu: 'Властивості медіа',
+        title: 'Медія',
+        urlMissing: 'URL-адреса відсутня'
+    });
+})(CKEDITOR);
